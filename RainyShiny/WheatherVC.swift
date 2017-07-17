1a8d6cf358e921cc831f1eff4c30eff70d1702a6
Sattar Hummatli <settarxan@gmail.com>
2017-07-17 16:45:17 +0400

    Created my new project about wheater. Installed my first CocoaPods AlamoFire for network requests, Desgned UI

---
+++ RainyShiny/WheatherVC.swift
@@ -0,0 +1,19 @@
+//
+//  WheatherVC.swift
+//  RainyShiny
+//
+//  Created by Settar Hummetli on 7/17/17.
+//  Copyright © 2017 Settar Hummetli. All rights reserved.
+//
+
+import UIKit
+
+class WheatherVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
+
