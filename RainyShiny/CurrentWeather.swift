aff5745c4ac860d43b12068c34da1342b1f1ca78
Sattar Hummatli <settarxan@gmail.com>
2017-07-17 20:15:00 +0400

    I have pared JSON data from response and now trying to display that informatin in screen

--- RainyShiny/CurrentWeather.swift
+++ RainyShiny/CurrentWeather.swift
@@ -60,8 +60,35 @@ class CurrentWeather {
         
         Alamofire.request(currentWeatherURL!).responseJSON { response in
             let result  = response.result
-            print("Request URL = \(CURRENT_WEATHER_URL)")
-            print("Result from request = \(response)")
+            //print("Request URL = \(CURRENT_WEATHER_URL)")
+            //print("Result from request = \(response)")
+            
+            if let dict = result.value as? Dictionary<String, AnyObject> {
+                if let name = dict["name"] as? String {
+                    self._cityName = name.capitalized
+                    print(self._cityName)
+                }
+                
+                if let weather = dict["weather"] as? [Dictionary<String, AnyObject>] {
+                    if let main = weather[0]["main"] as? String {
+                        self._weatherType = main.capitalized
+                        print(self._weatherType)
+                    }
+                }
+                
+                if let main = dict["main"] as? Dictionary<String, AnyObject> {
+                    if let temp = main["temp"] as? Double {
+                        
+                        let kelvinToFarenheitPreDivision = (temp * (9 / 5) - 459.67)
+                        
+                        let kelvinToFarenheit = Double(round(10 * kelvinToFarenheitPreDivision))
+                        
+                        self._currentTemp = kelvinToFarenheit
+                        
+                        print(self._currentTemp)
+                    }
+                }
+            }
             
         }
         completed()
