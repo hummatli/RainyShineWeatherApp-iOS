81acc86518ddff126c8a51bbbcec41c67e5746be
Sattar Hummatli <settarxan@gmail.com>
2017-07-17 17:27:50 +0400

    Created TableDataSource and Table delegate for weather tableview

--- RainyShiny/WheatherVC.swift
+++ RainyShiny/WheatherVC.swift
@@ -8,12 +8,36 @@
 
 import UIKit
 
-class WheatherVC: UIViewController {
-
+class WheatherVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var dateLbl: UILabel!
+    @IBOutlet weak var currentTempLbl: UILabel!
+    @IBOutlet weak var locationLbl: UILabel!
+    @IBOutlet weak var currentWeatherIV: UIImageView!
+    @IBOutlet weak var currentWeatherTypeLbl: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.delegate = self
+        tableView.dataSource = self
+        
     }
-
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "weatherCell", for: indexPath)
+        return cell
+    }
+    
 }
 
