05619524c8dc02b9edbc1c918f092e2d5aa43ab1
Sattar Hummatli <settarxan@gmail.com>
2017-07-18 10:04:50 +0400

    Grabbind data from forecast service

---
+++ RainyShiny/Forecast.swift
@@ -0,0 +1,9 @@
+//
+//  Forecast.swift
+//  RainyShiny
+//
+//  Created by Settar Hummetli on 7/18/17.
+//  Copyright © 2017 Settar Hummetli. All rights reserved.
+//
+
+import Foundation
---
+++ RainyShiny/Utils.swift
@@ -0,0 +1,9 @@
+//
+//  Utils.swift
+//  RainyShiny
+//
+//  Created by Settar Hummetli on 7/18/17.
+//  Copyright © 2017 Settar Hummetli. All rights reserved.
+//
+
+import Foundation
